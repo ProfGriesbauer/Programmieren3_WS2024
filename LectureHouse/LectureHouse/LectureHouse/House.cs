6babbca0071cf51fb80428b68b18183429485c0e
ProfGriesbauer <josef.griesbauer@hs-kempten.de>
2024-10-10 12:30:14 +0200

    LectureHouse first Version 10.10.2024

---
+++ LectureHouse/LectureHouse/LectureHouse/House.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LectureHouse
+{
+    public class House
+    {
+        float _StromV = 56; //in A
+
+        public float GibMirDenStromVerbauchInmA (House haus)
+        {
+            return _StromV*1000;
+        }
+    }
+}
---
+++ LectureHouse/LectureHouse/LectureHouse/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+LectureHouse.House testHaus = new LectureHouse.House();
+LectureHouse.House testHaus2 = new LectureHouse.House();
+int testInt = 8;
+
+Console.WriteLine("1: Hello, World! StromV: {0:F}", testHaus.GibMirDenStromVerbauchInmA(testHaus2));
+Console.WriteLine("2: Hello, World! StromV: {0:F}", testHaus2.GibMirDenStromVerbauchInmA(testHaus2));
