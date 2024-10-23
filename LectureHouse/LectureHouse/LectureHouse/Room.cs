dc0c5ed073788529d6a82800c91845fbdde51588
ProfGriesbauer <josef.griesbauer@hs-kempten.de>
2024-10-23 10:02:41 +0200

    31.10.2024 Verbesserung von dem vergessenen Room

---
+++ LectureHouse/LectureHouse/LectureHouse/Room.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LectureHouse
+{
+    public interface IRoom
+    {
+        public void AlleLichterAn();
+
+        public void AlleLichterAus();
+    }
+    public class Room : IRoom
+    {
+        IList<Light> _lights;
+
+        public Room ()
+        {
+            _lights = new List<Light>();
+            _lights.Add(new Light());
+            _lights.Add(new Light());
+            _lights.Add(new Light());
+            _lights.Add(new Light());
+        }
+        public void AlleLichterAn()
+        {
+            foreach (Light l in _lights)
+            {
+                l.OnOff = true;
+            }
+            return;
+        }
+
+        public void AlleLichterAus()
+        {
+            foreach (Light l in _lights)
+            {
+                l.OnOff = true;
+            }
+            return;
+        }
+    }
+
+    public class Light
+    {
+        bool _OnOff;
+
+        public bool OnOff
+        {
+            get { return _OnOff; }
+            set { _OnOff = value; }
+        }
+    }
+}
