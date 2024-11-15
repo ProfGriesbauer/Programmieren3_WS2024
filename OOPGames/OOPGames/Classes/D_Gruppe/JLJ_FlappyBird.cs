795c7038c1ddb8e72175f58d2474833b849144aa
Haegger <77164060+Haegger@users.noreply.github.com>
2024-11-15 08:22:10 +0100

    Interfaces und Klassen mit VS Schnitstellen implementieren lassen

--- OOPGames/OOPGames/Classes/D_Gruppe/JLJ_FlappyBird.cs
+++ OOPGames/OOPGames/Classes/D_Gruppe/JLJ_FlappyBird.cs
@@ -24,4 +24,78 @@ namespace OOPGames
             throw new NotImplementedException();
         }
     }
+
+    public class D_FB_GameField : ID_FB_GameField
+    {
+        public bool CanBePaintedBy(IPaintGame painter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class D_FB_Rules : ID_FB_Rules
+    {
+        public string Name => throw new NotImplementedException();
+
+        public IGameField CurrentField => throw new NotImplementedException();
+
+        public bool MovesPossible => throw new NotImplementedException();
+
+        public int CheckIfPLayerWon()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearField()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DoMove(IPlayMove move)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void StartedGameCall()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TickGameCall()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class D_FB_Move : ID_FB_Move
+    {
+        
+    }
+
+    public class D_FBHumanPlayer : ID_FB_HumanPlayer
+    {
+        public string Name => throw new NotImplementedException();
+
+        public int PlayerNumber => throw new NotImplementedException();
+
+        public bool CanBeRuledBy(IGameRules rules)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IGamePlayer Clone()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPlayMove GetMove(IMoveSelection selection, IGameField field)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetPlayerNumber(int playerNumber)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
