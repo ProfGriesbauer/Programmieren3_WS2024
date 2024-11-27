f89b49150f869e339017e798aacdc580d7dbe71d
Chris-Pres <christoph-prestel@t-online.de>
2024-11-27 11:03:07 +0100

    BV Regeln Painter und Player erneut regisitriert B_Move_BV hinzu

---
+++ OOPGames/OOPGames/Classes/B_Gruppe/Volley/B_Move_BV.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPGames
+{
+    internal class B_Move_BV : IB_Move_BV
+    {
+        public int PlayerNumber => throw new NotImplementedException();
+    }
+}
