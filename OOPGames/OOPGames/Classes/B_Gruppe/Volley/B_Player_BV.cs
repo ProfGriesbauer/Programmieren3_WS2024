80df57bbfb424478ffac0ed1c7eb8328a8573ba8
Fabi_R_tech <fabian.m.reimann@gmail.com>
2024-11-27 09:06:14 +0100

    Player Klasse

---
+++ OOPGames/OOPGames/Classes/B_Gruppe/Volley/B_Player_BV.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPGames.Classes.B_Gruppe.Volley
+{
+    internal class B_Player_BV
+    {
+    }
+}
