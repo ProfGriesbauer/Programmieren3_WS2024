6dd06f44cecccec4ebea973a031bbf3b45f5a5f0
Chris-Pres <christoph-prestel@t-online.de>
2024-11-27 08:23:49 +0100

    Added Rules and Player Classes for B_Blobby Volley

--- OOPGames/OOPGames/Classes/B_Gruppe/Volley/B_Painter_BV.cs
+++ OOPGames/OOPGames/Classes/B_Gruppe/Volley/B_Painter_BV.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
 
 namespace OOPGames
 {
@@ -11,10 +13,65 @@ namespace OOPGames
     {
         public string Name => "Blobby Volley Painter";
 
-        public void PaintBlobbyVolley(Canvas canvas, IB_Field_BV playField)
+        public void PaintBlobbyVolley(Canvas canvas, IB_Field_BV field)
         {
-            throw new NotImplementedException();
-        }
+            canvas.Children.Clear();
+
+            // Spielfeld zeichnen
+            Rectangle ground = new Rectangle
+            {
+                Width = canvas.ActualWidth,
+                Height = canvas.ActualHeight * 0.1,
+                Fill = Brushes.Green
+            };
+            Canvas.SetTop(ground, canvas.ActualHeight - ground.Height);
+            canvas.Children.Add(ground);
+
+            // Netz zeichnen
+            Rectangle net = new Rectangle
+            {
+                Width = 10,
+                Height = canvas.ActualHeight * 0.6,
+                Fill = Brushes.White
+            };
+            Canvas.SetLeft(net, canvas.ActualWidth / 2 - net.Width / 2);
+            Canvas.SetTop(net, canvas.ActualHeight - ground.Height - net.Height);
+            canvas.Children.Add(net);
+
+            // Spieler zeichnen
+            Ellipse player1 = new Ellipse
+            {
+                Width = 50,
+                Height = 50,
+                Fill = Brushes.Blue
+            };
+            Canvas.SetLeft(player1, field.Player1.Pos_x);
+            Canvas.SetTop(player1, field.Player1.Pos_y);
+            canvas.Children.Add(player1);
+
+            Ellipse player2 = new Ellipse
+            {
+                Width = 50,
+                Height = 50,
+                Fill = Brushes.Red
+            };
+            Canvas.SetLeft(player2, field.Player2.Pos_x);
+            Canvas.SetTop(player2, field.Player2.Pos_y);
+            canvas.Children.Add(player2);
+
+            // Ball zeichnen
+            Ellipse ball = new Ellipse
+            {
+                Width = 30,
+                Height = 30,
+                Fill = Brushes.Yellow
+            };
+            Canvas.SetLeft(ball, field.Ball.Pos_x);
+            Canvas.SetTop(ball, field.Ball.Pos_y);
+            canvas.Children.Add(ball);
+        
+         }
+
 
         public void PaintGameField(Canvas canvas, IGameField playField)
         {
