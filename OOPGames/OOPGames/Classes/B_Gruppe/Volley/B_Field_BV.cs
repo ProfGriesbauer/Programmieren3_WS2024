aeb90cbff20767ebae58e511154b7408e8cf03ea
louiis-k <louislinuskoehler@gmx.de>
2024-11-27 10:38:37 +0100

    commit

--- OOPGames/OOPGames/Classes/B_Gruppe/Volley/B_Field_BV.cs
+++ OOPGames/OOPGames/Classes/B_Gruppe/Volley/B_Field_BV.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+
+using OOPGames;
 
 namespace OOPGames
 {
@@ -16,79 +19,97 @@ namespace OOPGames
         private IB_Rules_BV _rules;
 
         public B_Field_BV(IB_Rules_BV rules)
+        {
             _height = 600;
             _width = 600;
-            _ball = new B_Ball_BV();
-            _player1 = new B_Player_BV();
-            _player2 =new B_Player_BV();
-          
+            _ball = new BV_Ball();
 
-        public B_Field_BV(IB_Rules_BV rules)
-        {
+            _player1 = new B_HumanPlayer_BV();
+            _player2 = new B_HumanPlayer_BV();
+
             _rules = rules;
         }
-        public double Height {
-            get { 
+
+        public double Height
+        {
+            get
+            {
                 return _height;
-            };
+            }
             set
             {
-                _height = value;    
-            } 
-        }
-        public double Width { get
-            {
-                return _width;
-            }; 
-            set
-            {
-                _width = value; 
-            } 
+                _height = value;
+            }
         }
 
-        public double MarginPercentage = 0,1;
+        public double Width
+        {
+            get
+            {
+                return _width;
+            }
+            set
+            {
+                _width = value;
+            }
+        }
+
+        public double MarginPercentage
+        {
+            get
+            {
+                return 0.1; // Beispielwert
+            }
+        }
 
         public IB_Ball_BV Ball
         {
             get
             {
-                return (IB_Ball_BV)_ball;
+                return _ball;
             }
             set
             {
                 _ball = value;
             }
         }
-        public IB_Player_BV Player1 { get
+
+        public IB_Player_BV Player1
+        {
+            get
             {
                 return _player1;
-            };
+            }
             set
             {
                 _player1 = value;
             }
         }
-        public IB_Player_BV Player2 { get
+
+        public IB_Player_BV Player2
+        {
+            get
             {
-                return _player2
+                return _player2;
             }
             set
             {
                 _player2 = value;
-            } 
+            }
         }
 
         public IB_Rules_BV Rules_BV
         {
             get
             {
-                return _rules
+                return _rules;
             }
         }
 
         public bool CanBePaintedBy(IPaintGame painter)
         {
-            return painter is IB_Player_BV;
+            return painter is IB_Painter_BV;
         }
     }
 }
+
